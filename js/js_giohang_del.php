d607ed67049e84dc07f5710073b93abd701ae856
hieunhph03712 <hieunhph03712@fpt.edu.vn>
2016-08-25 20:52:15 +0700

    upfile js bởi hieunhph03712

---
+++ js/js_giohang_del.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+include'../sql.php';
+if(isset($_GET['p_id']))
+mysql_query('delete from mua where c_id="'.$_SESSION['c_id'].'"&&p_id="'.$_GET['p_id'].'"');
+?>
---
+++ js/js_xoaUser.php
@@ -0,0 +1,6 @@
+<?php
+  include'../sql.php';
+   mysql_query('delete from mua where c_id="'.$_GET['c_id'].'"');   
+   if(mysql_query('delete from custom where c_id="'.$_GET['c_id'].'"'))
+   echo'ok'; 
+?>
